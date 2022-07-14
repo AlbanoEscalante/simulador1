b77c64801352ab4f3475af3cbf2185cb39425acd
Albano Escalante <albanoescalante@gmail.com>
2022-07-14 18:22:36 -0300

    nuevo contenido

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
 
-function promedio(){
- let apellido =prompt(`Ingresa tu apellido:`);
- apellido=apellido.toUpperCase()
- let nota1 =Number(prompt(`Ingresa Nota del primer parcial`));
- let nota2 =Number(prompt(`Ingresa Nota del segundo parcial`));
- let nota3 =Number(prompt(`Ingresa Nota del tercer parcial`));
- let notafinal =(nota1+nota2+nota3)/3;
-
-  if(notafinal>=7)
-  {
-   alert(apellido +  "APROBADO")
-  }
-  else if(notafinal<7)
-  {
-    alert(apellido + "DESAPROBADO")
-  }
-
+function Usuario (id, nombre, apellido)
+{
+    this.id=id;
+    this.nombre=nombre;
+    this.apellido=apellido;
 }
-promedio()
-
+const usuario1 = new Usuario (1,"albano","escalante")
+console.log(usuario1)
